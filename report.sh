3d5d9c0350468cc8742fa42b6a5484f9e4e372b6
Jason Evans <jsevans@opensuse.org>
2020-10-26 17:00:48 +0100

    fixed csv

--- report.sh
+++ report.sh
@@ -16,30 +16,30 @@ SIZE=`du -s /var/spool/news/articles | awk '{print $1}'`
 SIZEMB=`expr $SIZE / 1024`
 
 echo $NOW > /var/www/html/reports/$NOW.sizereport.txt
-printf "$ARTICLES" >> /var/www/html/reports/$NOW.sizereport.txt; printf ", Total number of articles:" >> /var/www/html/reports/$NOW.sizereport.txt
+printf "Total number of articles:," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$ARTICLES" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$ALT" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",alt." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "Current size in MB," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$SIZEMB" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$COMP" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",comp." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "alt.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$ALT" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$NEWS" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",news." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "free.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$FREE" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$MISC" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",misc." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "comp.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$COMP" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$SCI" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",sci." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "humanities.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$HUM" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$HUM" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",humanities." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "misc.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$MISC" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$REC" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",rec." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "news.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$NEWS" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$SOC" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",soc." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "rec.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$REC" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$TALK" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",talk." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "sci.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$SCI" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$FREE" >> /var/www/html/reports/$NOW.sizereport.txt; printf ",free." >> /var/www/html/reports/$NOW.sizereport.txt
+printf "soc.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$SOC" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-printf "$SIZEMB" >> /var/www/html/reports/$NOW.sizereport.txt; printf ", Current size in MB" >> /var/www/html/reports/$NOW.sizereport.txt
+printf "talk.," >> /var/www/html/reports/$NOW.sizereport.txt;printf "$TALK" >> /var/www/html/reports/$NOW.sizereport.txt
 echo >> /var/www/html/reports/$NOW.sizereport.txt
-cp -v /var/www/html/reports/$NOW.sizereport.txt /var/www/html/reports/sizereport.txt
-mv -v /var/www/html/reports/$NOW.sizereport.txt /var/www/html/reports/archive/$NOW.sizereport.txt
 
+cp -v /var/www/html/reports/$NOW.sizereport.txt /var/www/html/reports/sizereport.txt
+mv -v /var/www/html/reports/$NOW.sizereport.txt /var/www/html/reports/archive/$NOW.sizereport.txt 
