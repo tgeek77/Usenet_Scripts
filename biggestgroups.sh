eaf270044c4514e9a10d3b649b72129708f0d777
Jason Evans <jsevans@mailfence.com>
2020-10-26 12:07:34 +0100

    added archive function

--- biggestgroups.sh
+++ biggestgroups.sh
@@ -4,18 +4,25 @@ NOW=`date '+%F_%H:%M:%S'`
 
 echo "running big8 report"
 echo
-echo $NOW > /var/www/html/reports/mostarticles_big8.txt
+echo $NOW > /var/www/html/reports/$NOW.mostarticles_big8.txt
 find /var/spool/news/articles -xdev -type d -exec sh -c '
-    echo "$(find "$0" | grep "^$0/[^/]*$" | wc -l) $0"' {} \; | sort -rn | grep -E ''/var/spool/news/articles/comp'|'/var/spool/news/articles/news'|'/var/spool/news/articles/misc'|'/var/spool/news/articles/sci'|'/var/spool/news/articles/humanities'|'/var/spool/news/articles/rec'|'/var/spool/news/articles/soc'|'/var/spool/news/articles/talk'' >> /var/www/html/reports/mostarticles_big8.txt
+    echo "$(find "$0" | grep "^$0/[^/]*$" | wc -l) $0"' {} \; | sort -rn | grep -E ''/var/spool/news/articles/comp'|'/var/spool/news/articles/news'|'/var/spool/news/articles/misc'|'/var/spool/news/articles/sci'|'/var/spool/news/articles/humanities'|'/var/spool/news/articles/rec'|'/var/spool/news/articles/soc'|'/var/spool/news/articles/talk'' >> /var/www/html/reports/$NOW.mostarticles_big8.txt
+cp -v /var/www/html/reports/$NOW.mostarticles_big8.txt /var/www/html/reports/mostarticles_big8.txt
+mv -v /var/www/html/reports/$NOW.mostarticles_big8.txt /var/www/html/reports/archive/$NOW.mostarticles_big8.txt
 
 echo "running top50 report"
 echo
-echo $NOW > /var/www/html/reports/top50_mostarticles.txt
+echo $NOW > /var/www/html/reports/$NOW.top50_mostarticles.txt
 find /var/spool/news/articles -xdev -type d -exec sh -c '
-  echo "$(find "$0" | grep "^$0/[^/]*$" | wc -l) $0"' {} \; | sort -rn | head -50 >> /var/www/html/reports/top50_mostarticles.txt
+  echo "$(find "$0" | grep "^$0/[^/]*$" | wc -l) $0"' {} \; | sort -rn | head -50 >> /var/www/html/reports/$NOW.top50_mostarticles.txt
+cp -v /var/www/html/reports/$NOW.top50_mostarticles.txt /var/www/html/reports/top50_mostarticles.txt
+mv -v /var/www/html/reports/$NOW.top50_mostarticles.txt /var/www/html/reports/archive/$NOW.top50_mostarticles.txt
 
 echo "running top1000 report"
 echo
-echo $NOW > /var/www/html/reports/top1000_mostarticles.txt
+echo $NOW > /var/www/html/reports/$NOW.top1000_mostarticles.txt
 find /var/spool/news/articles -xdev -type d -exec sh -c '
-  echo "$(find "$0" | grep "^$0/[^/]*$" | wc -l) $0"' {} \; | sort -rn | head -1000 >> /var/www/html/reports/top1000_mostarticles.txt
+  echo "$(find "$0" | grep "^$0/[^/]*$" | wc -l) $0"' {} \; | sort -rn | head -1000 >> /var/www/html/reports/$NOW.top1000_mostarticles.txt
+cp -v /var/www/html/reports/$NOW.top1000_mostarticles.txt /var/www/html/reports/top1000_mostarticles.txt
+mv -v /var/www/html/reports/$NOW.top1000_mostarticles.txt /var/www/html/reports/archive/$NOW.top1000_mostarticles.txt
+
