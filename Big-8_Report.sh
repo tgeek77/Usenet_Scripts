7fecfe17dff954ff9922612be45a36373e1c37a8
Jason Evans <jevans@suse.com>
2021-01-15 15:51:58 +0100

    Update Big-8_Report.sh

--- Big-8_Report.sh
+++ Big-8_Report.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# This script will create a report of active Usenet groups in the big-8 hierarchies. 
+# It will create a text file in your home directory with a name similar to: "2021-01-15_15:47:47.mostarticles_big8.txt"
+# This script assumes you are using the tradspool storage format and that your articles are stored in /var/spool/news/articles.
 
 NOW=`date '+%F_%H:%M:%S'`
 
